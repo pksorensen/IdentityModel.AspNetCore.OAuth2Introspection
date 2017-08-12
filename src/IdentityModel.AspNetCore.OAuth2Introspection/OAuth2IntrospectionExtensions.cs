5fd31b4900cef9b7dbd49073c69accc4cc3b2e92
Poul Kjeldager Sørensen <pks@s-innovations.net>
2017-08-12 15:21:13 +0200

    moved to authentication handler

--- src/IdentityModel.AspNetCore.OAuth2Introspection/OAuth2IntrospectionExtensions.cs
+++ src/IdentityModel.AspNetCore.OAuth2Introspection/OAuth2IntrospectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityModel.AspNetCore.OAuth2Introspection;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -14,17 +15,19 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class OAuth2IntrospectionExtensions
     {
 
-        public static IServiceCollection AddOAuth2IntrospectionAuthentication(this IServiceCollection services) => services.AddOAuth2IntrospectionAuthentication(OAuth2IntrospectionDefaults.AuthenticationScheme);
+        public static AuthenticationBuilder AddOAuth2IntrospectionAuthentication(this AuthenticationBuilder builder) 
+            => builder.AddOAuth2IntrospectionAuthentication(OAuth2IntrospectionDefaults.AuthenticationScheme);
 
-        public static IServiceCollection AddOAuth2IntrospectionAuthentication(this IServiceCollection services, string authenticationScheme) => services.AddOAuth2IntrospectionAuthentication(authenticationScheme, configureOptions: null);
+        public static AuthenticationBuilder AddOAuth2IntrospectionAuthentication(this AuthenticationBuilder builder, string authenticationScheme) 
+            => builder.AddOAuth2IntrospectionAuthentication(authenticationScheme, configureOptions: null);
 
-        public static IServiceCollection AddOAuth2IntrospectionAuthentication(this IServiceCollection services, Action<OAuth2IntrospectionOptions> configureOptions) =>
-            services.AddOAuth2IntrospectionAuthentication(OAuth2IntrospectionDefaults.AuthenticationScheme, configureOptions);
+        public static AuthenticationBuilder AddOAuth2IntrospectionAuthentication(this AuthenticationBuilder builder, Action<OAuth2IntrospectionOptions> configureOptions) 
+            => builder.AddOAuth2IntrospectionAuthentication(OAuth2IntrospectionDefaults.AuthenticationScheme, configureOptions);
 
-        public static IServiceCollection AddOAuth2IntrospectionAuthentication(this IServiceCollection services, string authenticationScheme, Action<OAuth2IntrospectionOptions> configureOptions)
+        public static AuthenticationBuilder AddOAuth2IntrospectionAuthentication(this AuthenticationBuilder builder, string authenticationScheme, Action<OAuth2IntrospectionOptions> configureOptions)
         {
-            services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<OAuth2IntrospectionOptions>, PostConfigureOAuth2IntrospectionAuthenticationOptions>());
-            return services.AddScheme<OAuth2IntrospectionOptions,OAuth2IntrospectionHandler>(authenticationScheme, configureOptions);
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<OAuth2IntrospectionOptions>, PostConfigureOAuth2IntrospectionAuthenticationOptions>());
+            return builder.AddScheme<OAuth2IntrospectionOptions,OAuth2IntrospectionHandler>(authenticationScheme, configureOptions);
         }
 
     }
